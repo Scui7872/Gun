d805e73b07755688339bb072b7b87908f87a009e
Scui7872 <1610265352@qq.com>
2020-10-20 17:39:30 +0800

    实现用户登录/存储账号密码/toast提示相关信息

--- app/src/main/java/com/example/activity/gun/MainActivity.java
+++ app/src/main/java/com/example/activity/gun/MainActivity.java
@@ -6,14 +6,67 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText etUsername;
+    private EditText etPassword;
+    private CheckBox cbAutoLogin;
+    private Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etUsername = findViewById(R.id.tv_username);
+        etPassword = findViewById(R.id.tv_passwd);
+        cbAutoLogin = findViewById(R.id.tv_auto);
+        btnLogin = findViewById(R.id.tv_btn);
+        Map<String,String> data = SPUtil.getAll(this);
+        if(data.size() > 0) {
+            etUsername.setText(data.get("account"));
+            etPassword.setText(data.get("password"));
+        }
+
+        btnLogin.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                login();
+            }
+        });
+    }
+
+    private void login() {
+        //3.1 获取用户名和密码的值
+        String username = etUsername.getText().toString().trim();
+        String password = etPassword.getText().toString();
+        if(TextUtils.isEmpty(username) || TextUtils.isEmpty(password)) {
+            Toast.makeText(this,"用户名或密码都不能为空", Toast.LENGTH_LONG).show();
+            return;
+        }
+        //3.2 比较用户名和密码是否正确，并给出提示
+        if("android".equals(username) && "123456".equals(password)) {
+            Toast.makeText(this,"登陆成功",Toast.LENGTH_LONG).show();
+            Intent intent = new Intent(this,TodayActivity.class);
+            intent.putExtra("username",username);
+            startActivity(intent);
+
+            if(cbAutoLogin.isChecked()) {
+                if (SPUtil.saveAccount(this,username,password));
+                Toast.makeText(MainActivity.this,"存储且登陆成功",Toast.LENGTH_LONG).show();
+            } else {
+                SPUtil.clear(this);
+            }
+        } else {
+            Toast.makeText(this,"用户名或密码不正确",Toast.LENGTH_LONG).show();
+        }
     }
 
     public void onClick(View view) {
